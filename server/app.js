1c9c85e8db73f3a6a6f41f63ca596013e97d9afd
shinite <ssshambhavi8@gmail.com>
2018-02-01 16:14:14 +0530

    final changes

--- server/app.js
+++ server/app.js
@@ -3,14 +3,16 @@ const app= express();
 const mongoose = require('mongoose');
 const path = require('path');
 const search = require('./routes/search')
+const index = require('./routes/index')
 var cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use('/static', express.static(path.join(__dirname, 'public')))
+const publicPath = path.join(__dirname,'..', 'public')
+app.use(express.static(publicPath))
 
 const db = mongoose.createConnection('mongodb://127.0.0.1/wordsDB',(err,database)=>{
   if(err){
@@ -19,17 +21,22 @@ const db = mongoose.createConnection('mongodb://127.0.0.1/wordsDB',(err,database
     console.log("connection to database was sucessful");
   }
 })
-
-
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-});
+//
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect('http://' + req.hostname + req.url);
+//   } else {
+//     next();
+//   }
+// });
+app.get('*',(req,res)=>{
+ res.sendFile(path.join(publicPath,'index.html'))
+})
 
 search(app,db)
+// index(app);
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
