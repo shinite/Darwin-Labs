d793d74d60b537dcd192ab82945f5dd3f5130596
shinite <ssshambhavi8@gmail.com>
2018-02-06 20:48:57 +0530

    changed package.json and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env ==='production';
-  
+
   const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname,'public'),
+      path: path.join(__dirname,'public','dist'),
       filename:'bundle.js'
     },
     module:{
@@ -54,7 +54,8 @@ module.exports = (env) => {
     devtool:isProduction ? 'source-map': 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname,'public'),
-      historyApiFallback:true
+      historyApiFallback:true,
+      publicPath: '/dist/'
     }
   };
 }
